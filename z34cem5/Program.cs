a1d7ae8fc9de9aec866efacf6ec79af33a9e2a72
Unenova Tatyana <unenovatanya71@mail.ru>
2023-01-19 20:24:05 +0800

    з 34 семинар 5

---
+++ z34cem5/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+int [] array = new int[4];
+Ran(array);
+Print(array);
+Skolko(array);
+
+// Console.WriteLine(ZZrray));
+void Ran(int[] col)
+{
+    for (int index = 0; index < col.Length; index++)
+        col[index] = new Random().Next(100, 1000);
+}
+
+void Print(int[] col2)
+{
+    foreach (int item in col2)
+        Console.Write(item + " ");
+}
+
+void Skolko(int[] col3)
+{
+    int a = 0;
+    
+    for (int index = 0; index < col3.Length; index++)
+    {
+        if (col3[index]%2 == 0) a = a + 1;
+        
+    }
+    Console.WriteLine("");
+    Console.WriteLine($"Колво чётных чисел в массиве: {a}");
+}
